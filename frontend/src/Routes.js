efba381c0b0e3f3ccb01cfba910acd0d2a928640
MdAkashSheikh <nmakashsheikh@gmail.com>
2024-04-01 20:11:37 +0600

    e-shop

---
+++ frontend/src/Routes.js
@@ -0,0 +1,6 @@
+import LoginPage from '../src/pages/Login.jsx';
+
+
+export {
+    LoginPage,
+}
---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className='min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+            
+            
+        </div>
+    )
+}
+
+export default Login
